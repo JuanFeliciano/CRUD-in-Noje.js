066fc994aca7c799c71346660c11214b116eb3b3
JuanFeliciano <139025070+JuanFeliciano@users.noreply.github.com>
2023-11-27 09:54:57 -0300

    Projeto Finalizado

--- src/index.js
+++ src/index.js
@@ -1,38 +1,45 @@
-const express = require('express')
-const app = express()
-const port = 3000
-const mongoose = require('mongoose')
+const express = require("express");
+const app = express();
+const port = 3000;
+const mongoose = require("mongoose");
 
-mongoose.connect('mongodb+srv://Juan:juangamer@cluster0.8r0ivb0.mongodb.net/?retryWrites=true&w=majority')
-app.use(express.json())
+mongoose.connect(
+  "mongodb+srv://Juan:juangamer@cluster0.8r0ivb0.mongodb.net/?retryWrites=true&w=majority"
+);
+app.use(express.json());
 
-const Film = mongoose.model('Film', {
-    name: String,
-    description: String,
-})
+const Film = mongoose.model("Film", {
+  name: String,
+  description: String,
+});
 
+app.get("/", async (req, res) => {
+  const allfilm = await Film.find();
+  return res.send(allfilm);
+});
 
-app.get('/', async(req,res) => {
-    const allfilm = await Film.find()
-    return res.send(allfilm)
-})
+app.post("/", async (req, res) => {
+  const film = new Film({
+    name: req.body.name,
+    description: req.body.description,
+  });
+  await film.save();
+  return res.send(film);
+});
 
-app.post('/', async(req,res) => {
-    const newfilm = new Film({
-        name: req.body.name,
-        description: req.body.description,
-    })
-    await newfilm.save()
-    return res.send(newfilm)
-})
+app.delete("/:id", async (req, res) => {
+  const film = await Film.findByIdAndDelete(req.params.id);
+  return res.send(film);
+});
 
-app.delete('/:id', async(req,res) => {
-    const delfilm = await Film.findByIdAndDelete(req.params.id)
-    return res.send(delfilm)
-})
-
-
+app.put("/:id", async (req, res) => {
+  const film = await Film.findByIdAndUpdate(req.params.id, {
+    name: req.body.name,
+    description: req.body.description,
+  });
+  return res.send(film);
+});
 
 app.listen(port, () => {
-    console.log(`app acessado na port ${port}`)
-})
+  console.log(`app acessado na port ${port}`);
+});
